65bae1d488c1e2ccae4f90c7c9b7fcfe9f283d1d
kateduquesnay <13khd@queensu.ca>
2016-11-24 12:18:03 -0500

    Update capitalizer.c

--- capitalizer.c
+++ capitalizer.c
@@ -3,3 +3,22 @@
 //Katherine DuQuesnay 10138898
 //Adele McCallum 10093867
 //Dean Wilkins-Reeves 10176758
+
+//Question 3: Capitalizer
+
+
+#include <stdio.h>
+
+int main(void) {
+  
+  textpoem = fopen("poem.txt", "w+");
+  if(textpoem == NULL){
+    printf("file not found");
+  }//end if
+
+
+    
+  
+	return 0;
+}//end main
+                   //isalpha() --> check to see if arg is in the alphabet
